1528824465d0e4da4ea015e346340601fb92d91b
Mateus Costa <mateusdsilvacosta@outlook.com>
2021-07-07 13:25:58 -0300

    Requisitos da v1 - Versao Final

--- app/views/admin_view.py
+++ app/views/admin_view.py
@@ -1,8 +1,8 @@
-from flask import (Blueprint)
+from flask import (Blueprint, render_template, redirect)
 from flask_httpauth import HTTPBasicAuth
 from app.models.user_model import UserModel
 
-bp_admin = Blueprint("bp_admin", __name__, url_prefix='/admin')
+bp_admin = Blueprint("bp_admin", __name__, url_prefix='/admin', template_folder='templates')
 
 auth = HTTPBasicAuth()
 
@@ -14,13 +14,23 @@ def verify_password(username, password):
     try:
         user = UserModel.query.filter_by(email=username).first()
         if UserModel.verify_password(user, password):
-            return user.serialized["API_Key"]
+            return user.serialized
     except:
         pass
+
 
 
 
 @bp_admin.route('/')
 @auth.login_required
 def admin():
-    return "API Key: {}".format(auth.current_user())
+    user_info = auth.current_user()
+    return render_template("admin.html", data=user_info)
+
+
+
+
+@bp_admin.route('/logout')
+@auth.login_required
+def logout():
+    return redirect("/admin/"), 401
